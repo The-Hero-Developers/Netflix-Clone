779c8f6500bef2e820b34e8644319bc2318124da
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-19 18:09:18 +0600

    new file and data fetch

---
+++ components/Banner.tsx
@@ -0,0 +1,9 @@
+function Banner() {
+  return <div>
+    <div>
+         
+    </div>
+  </div>;
+}
+
+export default Banner;
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,29 @@
-
-
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
-
-
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { BellIcon } from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 function Header() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true)
+      } else {
+        setIsScrolled(false)
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [])
+
   return (
-    <header>
+    <header className={`${isScrolled && "bg-[#141414]"}`}>
       <div className="flex items-center space-x-2 md:space-x-10">
         <img
           src="https://rb.gy/ulxxee"
@@ -26,10 +43,17 @@ function Header() {
         </ul>
       </div>
 
-      <div>
-        <MagnifyingGlassIcon className='hidden sm:inline h-6 w-6 '/>
-     
-
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden sm:inline h-6 w-6 " />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt=""
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   );
