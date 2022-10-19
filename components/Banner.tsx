cff4e7eb806a41c5c64e88cc169fdde8371f53b5
Masum-Arifin <masumarifinbd@gmail.com>
2022-10-19 23:40:46 +0600

    new file movie adedd

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,8 @@
-
-
 import  {Movie}  from  '../typings'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import Image from 'next/image';
+import { baseUrl } from '../constants/movie';
 
 interface  Props {
   netflixOriginals: Movie[]
@@ -10,9 +12,19 @@ interface  Props {
 
 
 function Banner({ netflixOriginals}: Props) {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[
+        Math.floor(Math.random() * netflixOriginals.length)
+      ]
+    )
+  }, [netflixOriginals])
+  console.log(movie)
   return <div>
     <div>
-         
+         <Image src={`${baseUrl}$movie?.backdrop_path || movie?.backdrop_path`}  />
     </div>
   </div>;
 }
