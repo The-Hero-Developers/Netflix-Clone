008a15fea01c5e495891b3643d7b60e68a2adebd
Masum-Arifin <masumarifinbd@gmail.com>
2022-11-19 15:02:37 +0600

    added login image

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
 import React from 'react'
 
 function login() {
   return (
-    <div>login</div>
+    <div>
+      <Head>
+        <title>Netflix</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Image
+        src="https://rb.gy/p2hphi"
+        layout="fill"
+        className="-z-10 !hidden opacity-60 sm:!inline"
+        objectFit="cover"
+      />
+    </div>
   )
 }
 
